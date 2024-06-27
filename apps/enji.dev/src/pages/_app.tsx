20241938f1f01e49ad2fc12a626c267262c571d0
delBull <marco.munoz9@gmail.com>
2024-06-27 14:05:57 -0600

    block copy text or saving images

--- apps/enji.dev/src/pages/_app.tsx
+++ apps/enji.dev/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { GoogleAnalytics } from '@next/third-parties/google';
+import { useEffect } from 'react';
 
 import RootLayout from '@/components/layouts/Root';
 import WithNavigationFooter from '@/components/layouts/WithNavigationFooter';
@@ -8,7 +9,10 @@ import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 import type { ReactElement, ReactNode } from 'react';
 
+
+
 import '@/styles/main.css';
+import '@/styles/globals.css';
 
 type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -32,6 +36,23 @@ function App({ Component, pageProps, router }: AppPropsWithLayout) {
   } else {
     getLayout = getDefaultLayout;
   }
+
+  useEffect(() => {
+    // Importar el archivo disableActions.js
+    const script = document.createElement('script');
+    script.src = '/disableActions.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Aplicar la clase no-select al body
+    document.body.classList.add('no-select');
+
+    // Eliminar el script y la clase cuando el componente se desmonte
+    return () => {
+      document.body.removeChild(script);
+      document.body.classList.remove('no-select');
+    };
+  }, []);
 
   return (
     <Provider>
