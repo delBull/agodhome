b5945e50130dd94142e7b1300c2de2fd3106018d
Jesus Bautista <jesbam98@gmail.com>
2024-09-09 17:31:28 -0600

    WIP: Waitlist

--- apps/enji.dev/README.md
+++
@@ -1,3 +0,0 @@
-<div align="center">
-  <h1>agodecosystem</h1>
-</div>
--- apps/enji.dev/declarations.d.ts
+++
@@ -1,5 +0,0 @@
-// src/declarations.d.ts (o cualquier nombre de tu elección, asegurándote que tenga extensión .d.ts)
-declare module '*.mp4' {
-  const src: string;
-  export default src;
-}
