2ce330a3af2caf0f63c1eab71a1837efe532ae22
delBull <marco.munoz9@gmail.com>
2024-11-24 16:54:46 -0600

    privacidad updated

--- apps/agodhome/src/contents/privacidad.tsx
+++ apps/agodhome/src/contents/privacidad.tsx
@@ -1,178 +1,108 @@
 import clsx from 'clsx';
-
 import { SectionButton } from '@/components/sections/SectionButton';
 import SectionContent from '@/components/sections/SectionContent';
 import SectionTitle from '@/components/sections/SectionTitle';
 
 function Privacidad() {
-
   return (
     <>
-    <SectionContent>
+      <SectionContent>
         <div className={clsx('-mt-10 flex')}>
           <SectionButton
             title="Introducción."
             icon=""
-            description="Bienvenidos a AGOD Ecosystem. Nos comprometemos a proteger su privacidad y a asegurar que su información personal se maneje de manera segura y responsable. Esta Política de Privacidad describe cómo recopilamos, utilizamos, divulgamos y protegemos su información cuando interactúa con nuestra plataforma y servicios."
+            description="Bienvenidos a AGOD Ecosystem. Nos comprometemos a proteger su privacidad y asegurar que su información personal se maneje de manera segura y responsable. Esta Política de Privacidad describe cómo recopilamos, utilizamos, divulgamos y protegemos su información al interactuar con nuestra plataforma y servicios."
           />
         </div>
       </SectionContent>
+
       <SectionTitle
-        title="Información Personal"
-        caption="Información que recopilamos"
-        description="Nombre completo, 
-Dirección de correo electrónico, 
-Información de contacto, 
-Identificación personal (para verificación de identidad)."
+        title="Información que recopilamos"
+        caption="Incluye datos personales y técnicos"
+        description="Recopilamos información personal (nombre, correo electrónico, información de contacto, identificación personal), información técnica (dirección IP, tipo de navegador, sistema operativo, páginas visitadas) y datos relacionados con el uso de nuestra plataforma (actividad, participación en eventos, uso de tokens)."
         altdesc
       />
-      <div style={{marginTop: '30px'}} />
+      <div style={{ marginTop: '30px' }} />
+
       <SectionTitle
-        title="Información Técnica"
+        title="Cómo utilizamos su información"
+        caption="Propósitos principales"
+        description="Utilizamos su información para proporcionar y mejorar nuestros servicios, facilitar comunicaciones, cumplir con obligaciones legales, realizar investigaciones y desarrollo, y personalizar su experiencia en nuestras plataformas."
+        altdesc
+      />
+      <div style={{ marginTop: '30px' }} />
+
+      <SectionTitle
+        title="Uso de cookies y tecnologías similares"
         caption=""
-        description="Dirección IP, 
-Tipo de navegador y sistema operativo, 
-Páginas visitadas en nuestro sitio web, 
-Duración y frecuencia de visitas."
+        description="Utilizamos cookies para personalizar su experiencia, analizar el tráfico del sitio y recordar sus preferencias. También empleamos tecnologías similares para optimizar la funcionalidad de nuestras plataformas. Puede gestionar sus preferencias de cookies desde la configuración de su navegador."
         altdesc
       />
-      <div style={{marginTop: '30px'}} />
+      <div style={{ marginTop: '30px' }} />
+
       <SectionTitle
-        title="Información Financiera"
+        title="Uso de Google reCAPTCHA"
+        caption="Protección contra bots"
+        description="Utilizamos Google reCAPTCHA para proteger nuestros servicios contra el acceso automatizado malicioso. Al usar nuestra plataforma, acepta que Google recopile datos personales según lo establecido en su Política de Privacidad y Términos de Servicio."
+        altdesc
+      />
+      <div style={{ marginTop: '30px' }} />
+
+      <SectionTitle
+        title="Compartir información"
+        caption="Terceros y obligaciones legales"
+        description="Podemos compartir su información con proveedores de servicios, socios comerciales y autoridades legales cuando sea necesario para cumplir con la ley o mejorar nuestros servicios. Siempre que se requiera, solicitaremos su consentimiento explícito."
+        altdesc
+      />
+      <div style={{ marginTop: '30px' }} />
+
+      <SectionTitle
+        title="Seguridad de la información"
+        caption="Protección de datos"
+        description="Implementamos medidas de seguridad técnicas y organizativas, como encriptación, firewalls y controles de acceso, para proteger su información contra el acceso no autorizado, pérdida o alteración."
+        altdesc
+      />
+      <div style={{ marginTop: '30px' }} />
+
+      <SectionTitle
+        title="Retención de datos"
         caption=""
-        description="Información de pago, 
-Historial de transacciones."
+        description="Retenemos su información solo durante el tiempo necesario para cumplir con los propósitos establecidos en esta Política de Privacidad, salvo que la ley exija lo contrario."
         altdesc
       />
-      <div style={{marginTop: '30px'}} />
+      <div style={{ marginTop: '30px' }} />
+
       <SectionTitle
-        title="Información de Uso"
+        title="Sus derechos"
+        caption="Acceso, corrección y eliminación"
+        description="Tiene derecho a acceder, actualizar o eliminar su información personal. También puede oponerse al procesamiento de su información o solicitar su portabilidad."
+        altdesc
+      />
+      <div style={{ marginTop: '30px' }} />
+
+      <SectionTitle
+        title="Términos y condiciones"
+        caption="Reglas del uso de nuestros servicios"
+        description="El uso de nuestra plataforma está sujeto a los términos y condiciones detallados en este documento. Incluyen las reglas de conducta del usuario, limitaciones de responsabilidad y otros aspectos legales clave. Revise los Términos y Condiciones completos para más información."
+        altdesc
+      />
+      <div style={{ marginTop: '30px' }} />
+
+      <SectionTitle
+        title="Cambios en la política de privacidad"
         caption=""
-        description="Actividad dentro de nuestras plataformas, 
-Participación en eventos, Uso de tokens."
+        description="Podemos actualizar esta Política de Privacidad para reflejar cambios en nuestras prácticas o requisitos legales. Notificaremos cualquier cambio significativo mediante nuestras plataformas o por correo electrónico."
         altdesc
       />
-      <div style={{marginTop: '30px'}} />
+      <div style={{ marginTop: '30px' }} />
+
       <SectionTitle
-        title="Para Proporcionar y Mejorar Nuestros Servicios"
-        caption="Cómo Utilizamos su Información"
-        description="Procesar sus transacciones y gestionar su cuenta, 
-Facilitar la autenticación y el acceso seguro mediante AGOD Key, 
-Mejorar la funcionalidad de nuestras plataformas y personalizar su experiencia."
+        title="Contacto"
+        caption="Cómo comunicarse con nosotros"
+        description="Si tiene preguntas o inquietudes, por favor contacte a nuestro equipo de privacidad en: privacidad@agodecosystem.com."
         altdesc
       />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title="Para Comunicaciones"
-        caption=""
-        description="Enviar notificaciones importantes sobre su cuenta y nuestras plataformas, 
-Proporcionar actualizaciones y noticias sobre el AGOD Ecosystem, 
-Responder a sus consultas y proporcionar soporte al cliente."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title="Para Cumplimiento Legal"
-        caption=""
-        description="Verificar su identidad y prevenir fraudes, 
-Cumplir con obligaciones legales y regulatorias."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title="Para Investigación y Desarrollo"
-        caption=""
-        description="Analizar el uso de nuestras plataformas para mejorar nuestros servicios, 
-Desarrollar nuevas funcionalidades y productos basados en el feedback y el comportamiento de los usuarios."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title="Con Terceros de Confianza"
-        caption="Cómo Compartimos su Información"
-        description="Proveedores de servicios que nos ayudan a operar y mejorar nuestras plataformas (proveedores de pagos, servicios de hosting, etc.), 
-socios comerciales y afiliados para mejorar la interoperabilidad y la experiencia del usuario."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title="Por Obligación Legal"
-        caption=""
-        description="Autoridades gubernamentales y reguladoras cuando sea requerido por ley."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title="Con su Consentimiento"
-        caption=""
-        description="Otros terceros con su consentimiento explícito para fines específicos."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title=""
-        caption="Seguridad de su Información"
-        description="Implementamos medidas de seguridad técnicas y organizativas para proteger su información personal contra el acceso no autorizado, la divulgación, alteración o destrucción. Estas medidas incluyen encriptación, firewalls y controles de acceso seguros."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title=""
-        caption="Retención de Datos"
-        description="Retenemos su información personal solo durante el tiempo necesario para cumplir con los propósitos descritos en esta Política de Privacidad, a menos que se requiera o permita un período de retención más largo por ley."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title="Acceso y Correción"
-        caption="Sus Derechos"
-        description="Puede acceder y actualizar su información personal en cualquier momento a través de su cuenta en nuestra plataforma."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title="Eliminación y Restricción"
-        caption=""
-        description="Puede solicitar la eliminación o restricción del procesamiento de su información personal, sujeto a ciertas condiciones legales y contractuales."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title="Portabilidad de Datos"
-        caption=""
-        description="Tiene derecho a recibir una copia de su información personal en un formato estructurado y de uso común."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title="Objeción"
-        caption=""
-        description="Puede oponerse al procesamiento de su información personal para fines específicos, como marketing directo."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title=""
-        caption="Cookies y Tecnologías Similares"
-        description="Utilizamos cookies y tecnologías similares para mejorar su experiencia en nuestras plataformas. Estas tecnologías nos permiten recordar sus preferencias, analizar el tráfico del sitio y personalizar contenido. Puede gestionar sus preferencias de cookies a través de la configuración de su navegador."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title=""
-        caption="Cambos a esta Política de Privacidad"
-        description="Podemos actualizar esta Política de Privacidad periódicamente para reflejar cambios en nuestras prácticas de información. Publicaremos cualquier actualización en esta página y, si los cambios son significativos, le notificaremos a través de nuestras plataformas o por correo electrónico."
-        altdesc
-      />
-      <div style={{marginTop: '30px'}} />
-      <SectionTitle
-        title=""
-        caption="Contacvto"
-        description="Si tiene alguna pregunta o inquietud sobre esta Política de Privacidad, por favor contacte a nuestro equipo de privacidad en: 
-privacidad@agodecosystem.com"
-        altdesc
-      />
-     </>
+    </>
   );
 }
 
