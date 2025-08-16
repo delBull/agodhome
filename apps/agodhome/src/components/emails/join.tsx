fa843b53414d8f675e29f7fb2efe4d08212d18bf
delBull <marco.munoz9@gmail.com>
2025-08-16 12:39:19 -0600

    imgs email routes fixed

--- apps/agodhome/src/components/emails/join.tsx
+++ apps/agodhome/src/components/emails/join.tsx
@@ -15,9 +15,7 @@ import {
 } from "@react-email/components";
 import * as React from "react";
 
-const baseUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "";
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "";
 
 export const Join = ({
   username,
